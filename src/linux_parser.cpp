8eadc0e0384592e90d7edb993c9b52ea39e84e10
Lillian <lillianmliang@gmail.com>
2020-02-05 16:45:32 +0000

    Add comment attributing CpuUtilization calc

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -205,6 +205,7 @@ long LinuxParser::UpTime(int pid) {
   return 0;
 }
 
+// Used implementation from https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
 float LinuxParser::CpuUtilization(int pid) {
   int utime; //14
   int stime; //15
